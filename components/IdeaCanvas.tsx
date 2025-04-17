128bc87d552f68a24d21353a745ad2602707c6e4
Justin Kwan <justinkkwan@alumni.ubc.ca>
2025-04-17 00:44:35 -0700

    Update to newer @shopify/react-native-skia package. Started work in TextRepresentation

--- components/IdeaCanvas.tsx
+++ components/IdeaCanvas.tsx
@@ -5,30 +5,16 @@ import TextRepresentation from "./ideaVisualization/TextRepresentation";
 
 export default () => {
     const canvasSize = useSharedValue({ width: 0, height: 0 });
-    
-    let roundedRectProperties: RoundedRectProps = {
-        x: 20,
-        y: 20,
-        height: 80,
-        width: 120,
-        r: 0,
-        color: "purple"
-    };
 
-    
     return (
-        <Canvas style={styles.canvas} onSize={ canvasSize }>
-            <RoundedRect {...roundedRectProperties}>
-
-            </RoundedRect>
-            <TextRepresentation />
+        <Canvas style={styles.canvas} onSize={canvasSize}>
+            <TextRepresentation text={'If a temporary buffer is not allowed, I would iterate through the list with two pointers: one (current) traversing the list, and a second (runner) that checks all nodes after current for duplicates. If a duplicate is found, I\'d remove it by adjusting the Next pointer of the previous node. This approach takes O(n²) time but only O(1) space.'} />
         </Canvas>
     );
 };
 
 const styles = StyleSheet.create({
     canvas: {
-        flex: 1,
-        backgroundColor: "blue"
+        flex: 1
     }
 });
--- components/ideaVisualization/TextRepresentation.tsx
+++ components/ideaVisualization/TextRepresentation.tsx
@@ -1,15 +1,82 @@
-import { useColorScheme } from 'react-native';
-import { Text, RoundedRect, matchFont } from '@shopify/react-native-skia';
+import { Platform, useColorScheme } from 'react-native';
+import { useMemo } from 'react';
+import { Paragraph, Skia, FontStyle, useFonts, TextAlign, rrect, rect, DiffRect, Text } from '@shopify/react-native-skia';
 
-const TextRepresentation = () => {
-    const fontStyle = {
-        fontFamily: "serif",
-        fontSize: 12
-    }
-    const font = matchFont(fontStyle);
+export interface TextRepresentationProps {
+    text: string;
+};
+
+export const TextRepresentation = (props: TextRepresentationProps) => {
+    const fontProvider = useFonts({
+        SpaceMono: [
+            Platform.OS === 'web' ?
+                { default: require('../../assets/fonts/SpaceMono-Regular.ttf') } :
+                require('../../assets/fonts/SpaceMono-Regular.ttf')] //Workaround for react-native-skia issue: https://github.com/Shopify/react-native-skia/issues/2784
+    });
+
+    const drawColor = useColorScheme() === 'dark' ?
+        Skia.Color('#888') :
+        Skia.Color('#000');
+
+    const paragraph = useMemo(() => {
+        if (!fontProvider) {
+            return null;
+        }
+
+        return Skia.ParagraphBuilder.Make({ textAlign: TextAlign.Center }, fontProvider)
+            .pushStyle({
+                fontSize: 24,
+                fontFamilies: ['SpaceMono'],
+                color: drawColor,
+                fontStyle: FontStyle.Normal
+            })
+            .addText(props.text)
+            .build();
+    }, [props, drawColor, fontProvider]);
+
+    if (!paragraph) return null;
+
+    //Trigger calculation of max width
+    paragraph.layout(1000);
+
+    const paragraphLineMetrics = paragraph.getLineMetrics() || [];
+    const combinedLineMetrics: { w: number, h: number } = paragraphLineMetrics.reduce((previous, skRect) => ({ w: previous.w + skRect.width, h: skRect.height }), { w: 0, h: 0 });
+
+    const scaleFactor = Math.floor(Math.sqrt(combinedLineMetrics.w / combinedLineMetrics.h));
+    const squareWidth = Math.floor(combinedLineMetrics.w / scaleFactor);
+
+    paragraph.layout(squareWidth);
+    const paragraphWidth = paragraph.getMaxWidth();
+    const paragraphHeight = paragraph.getHeight();
+
+    console.log(`ParaW = ${paragraphWidth}, ParaH = ${paragraphHeight}`);
+
+    //Draw a rounded rectange around the text
+
+    const padding = 10;
+    const boarderThickness = 5;
+    const posX = 500;
+    const posY = 500;
+
+    const innerRectWidth = paragraphWidth + padding * 2;
+    const innerRectHeight = paragraphHeight + padding * 2;
+    const innerRectX = posX - innerRectWidth / 2;
+    const innerRectY = posY - innerRectHeight / 2;
+    const innerRectCornerRadius = 5;
+
+    const outerRectWidth = innerRectWidth + boarderThickness * 2;
+    const outerRectHeight = innerRectHeight + boarderThickness * 2;
+    const outerRectX = posX - outerRectWidth / 2;
+    const outerRectY = posY - outerRectHeight / 2;
+    const outerRectCornerRadius = innerRectCornerRadius + boarderThickness;
+
+    const innerRect = rrect(rect(innerRectX, innerRectY, innerRectWidth, innerRectHeight), innerRectCornerRadius, innerRectCornerRadius);
+    const outerRect = rrect(rect(outerRectX, outerRectY, outerRectWidth, outerRectHeight), outerRectCornerRadius, outerRectCornerRadius);
+
     return (
         <>
-            <RoundedRect x={200} y={300} width={500} height={80} />
+            <Paragraph paragraph={paragraph} x={posX-paragraphWidth/2} y={posY-paragraphHeight/2} width={paragraphWidth} />
+            <DiffRect inner={innerRect} outer={outerRect} color={drawColor} />
         </>
     );
 };

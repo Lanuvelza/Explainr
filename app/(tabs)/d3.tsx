07b208ffdf6f6485d157d8482fc1e1ddce91f439
Lanuvelza <iantyylam@gmail.com>
2025-05-15 21:13:40 -0700

    WIP: explore d3

---
+++ app/(tabs)/d3.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const D3View = () => {
+    return (
+        <View>
+            <Text style={styles.text}>D3 Test</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'white'
+    }
+})
+
+export default D3View; 
